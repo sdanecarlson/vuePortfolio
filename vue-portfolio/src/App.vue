4d447b5ca301e5559b238c678e279e14a3c4b6ee
Dane Carlson <sdanecarlson@gmail.com>
2020-12-16 19:56:52 -0700

    Initial Commit

---
+++ vue-portfolio/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    <router-view />
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer'
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  background-color: #16171a;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: white;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue-portfolio/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="footer">
+        <footer class="container py-5">
+            <div class="row">
+                <div class="col-12 col-md">
+                <small class="d-block mb-3 text-muted">©1997 <br> </small>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.footer{
+    background-color: #040c1a;
+    height: 200px;
+}
+</style>
